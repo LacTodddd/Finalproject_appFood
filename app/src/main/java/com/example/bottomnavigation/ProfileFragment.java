2922247f4932a9e36ba8d4943435b2d97fb45b22
kritaAu <kritapat.au@gmail.com>
2023-11-25 18:01:57 +0700

    เพิ่ม food

--- app/src/main/java/com/example/bottomnavigation/ProfileFragment.java
+++ app/src/main/java/com/example/bottomnavigation/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.example.bottomnavigation;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,13 +8,21 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class ProfileFragment extends Fragment {
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        TextView btn = view.findViewById(R.id.logoutbtn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), Login.class));
+            }
+        });
+
+        return view;
     }
 }
