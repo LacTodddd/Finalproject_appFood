1fe44578c695ba23b49eea29832544d4ec11e342
kritaAu <kritapat.au@gmail.com>
2023-11-26 16:10:20 +0700

    scollview1

--- app/src/main/java/com/example/bottomnavigation/FoodFragment.java
+++ app/src/main/java/com/example/bottomnavigation/FoodFragment.java
@@ -10,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.TextView;
 
 import com.example.bottomnavigation.databinding.FragmentFoodBinding;
 
@@ -19,10 +20,17 @@ import java.util.ArrayList;
 public class FoodFragment extends Fragment {
 
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_food, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_food, container, false);
+
+        TextView btn = view.findViewById(R.id.text11);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), PizzaData.class));
+            }
+        });
+
+        return view;
     }
 }
